4541f7af276885e65459be0519d360fd406bbbc5
telesyk <telesyk89@gmail.com>
2022-11-28 13:07:00 +0200

    Extend dependencies: eslint, prettier, tailwindcss, postcss, autoprefixer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-6xl font-bold underline">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
